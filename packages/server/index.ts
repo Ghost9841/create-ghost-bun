76cc7fd726c70b324d331b8411ad0e4718abfdbf
Ghost9841 <asubedi778@gmail.com>
2025-09-01 16:05:01 +0545

    changes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,2 @@
-console.log("Hello via Bun!");
+console.log("Hello via Ghost Starter Application of Bun IN Server");
+console.log("You can edit the /packages/server/index.ts file to change this message");
